c2f11452443487e589791737d09148172a384d28
Piotr Szumański <134852062+pszumanski@users.noreply.github.com>
2024-12-20 20:25:16 +0100

    UsosPaymentsUserAccount bugfix
    UsosPaymentsUserAccount bugfix
    
    GitOrigin-RevId: 66008fdc832ae81c21f2080435367a5316d5ef55

--- core/src/main/java/pl/wmsdev/usos4j/model/payments/UsosPaymentsUserAccount.java
+++ core/src/main/java/pl/wmsdev/usos4j/model/payments/UsosPaymentsUserAccount.java
@@ -1,10 +1,8 @@
 package pl.wmsdev.usos4j.model.payments;
 
-import pl.wmsdev.usos4j.docs.NotTested;
-import pl.wmsdev.usos4j.docs.NotTestedReason;
+import pl.wmsdev.usos4j.model.common.UsosLocalizedString;
 import pl.wmsdev.usos4j.model.common.UsosObject;
 import pl.wmsdev.usos4j.model.fac.UsosFacultySimple;
 
-@NotTested(reason = NotTestedReason.NO_DATA)
-public record UsosPaymentsUserAccount(String accountNumber, String name, String titlePrefix,
+public record UsosPaymentsUserAccount(String accountNumber, UsosLocalizedString name, String titlePrefix,
                                       UsosFacultySimple faculty, UsosPaymentsCurrency currency, String swift) implements UsosObject {}
